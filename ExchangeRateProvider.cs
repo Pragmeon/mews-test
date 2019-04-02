aac2d6c480da93a7ddddf0217923e5405f0f4fc7
Pragmeon <49210274+Pragmeon@users.noreply.github.com>
2019-04-02 23:33:39 +0200

    Add files via upload

---
+++ ExchangeRateProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ExchangeRateUpdater
+{
+    public class ExchangeRateProvider
+    {
+        /// <summary>
+        /// Should return exchange rates among the specified currencies that are defined by the source. But only those defined
+        /// by the source, do not return calculated exchange rates. E.g. if the source contains "EUR/USD" but not "USD/EUR",
+        /// do not return exchange rate "USD/EUR" with value calculated as 1 / "EUR/USD". If the source does not provide
+        /// some of the currencies, ignore them.
+        /// </summary>
+        public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
+        {
+            List<ExchangeRate> exchangeRates = new List<ExchangeRate>();
+
+            ICustomExchangeProvider provider = new CNBProvider();
+            exchangeRates.AddRange(provider.GetData("CZK", currencies).Result);
+
+            return exchangeRates;
+        }
+    }
+}
